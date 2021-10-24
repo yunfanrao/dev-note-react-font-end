fa8ae73f204889e758b882a4d8892b2c5a9353f2
YZR0009 <yzr0009@tigermail.auburn.edu>
2021-10-23 20:45:22 -0400

    update folder name

--- src/components/routes/Signin.jsx
+++ src/components/routes/Signin.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../header/Header";
-import Signin from "../signin/Signin";
+import Signin from "../auth/Signin";
 
 function Home() {
   return (
--- src/components/routes/Signup.jsx
+++ src/components/routes/Signup.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../header/Header";
-import Signup from "../signin/Signup";
+import Signup from "../auth/Signup";
 
 function Home() {
   return (
